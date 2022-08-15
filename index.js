8da3f608bb1650916e0363f6469a8fdd28718e09
Joseph Sutera <joesutera11@gmail.com>
2022-08-15 15:11:26 -0400

    commit tree 25650f9f0ae11d8749c3c1d4ea7f1329b13c5c3e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+ 
+const submitData=(userName, userEmail)=>{
+const configurationObject = {
+    method:"POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+            },
+            body: JSON.stringify({name:userName,email:userEmail}),
+          };
+const bodyId= document.querySelector("body");
+    fetch("http://localhost:3000/users",configurationObject)
+    .then(response=>response.json())
+    .then(data=>bodyId.innerHTML=data.id)
+    .catch(function (error){
+        console.log(error);
+    })
+   
+}
